68c56c308faaafdaccbfe6bb03bd3fbd0a699bc4
MIRSENGAR <92634638+Mirsengar@users.noreply.github.com>
2022-06-30 02:05:40 +0430

    Update Request.php

--- Request.php
+++ Request.php
@@ -1,12 +1,12 @@
 <?php
 require_once("ZarinpalFunction.php");
-$MerchantID 	= "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+$MerchantID 		= "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 $Amount 		= 100;
-$Description 	= "تراکنش زرین پال";
-$Email 		= "";
+$Description 		= "تراکنش زرین پال";
+$Email 			= "";
 $Mobile 		= "";
-$CallbackURL 	= "http://example.com/verify.php";
-$ZarinGate 	= false;
+$CallbackURL 		= "http://example.com/verify.php";
+$ZarinGate 	        = false;
 $SandBox 		= false;
 $zp 			= new zarinpal();
 $result 		= $zp->request($MerchantID, $Amount, $Description, $Email, $Mobile, $CallbackURL, $SandBox, $ZarinGate);
@@ -16,6 +16,6 @@ if (isset($result["Status"]) && $result["Status"] == 100) {
 } else {
 	// error
 	echo "خطا در ایجاد تراکنش";
-	echo "<br />کد خطا : " . $result["Status"];
+	echo "<br />کد خطا : " 		. $result["Status"];
 	echo "<br />تفسیر و علت خطا : " . $result["Message"];
 }
