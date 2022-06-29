d3304c4395bda41d080aaac67938864168925a1b
mirsengar <Mirsengar>
2022-06-30 01:52:13 +0430

    first commit

---
+++ Request.php
@@ -0,0 +1,21 @@
+<?php
+require_once("ZarinpalFunction.php");
+$MerchantID 	= "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+$Amount 		= 100;
+$Description 	= "تراکنش زرین پال";
+$Email 		= "";
+$Mobile 		= "";
+$CallbackURL 	= "http://example.com/verify.php";
+$ZarinGate 	= false;
+$SandBox 		= false;
+$zp 			= new zarinpal();
+$result 		= $zp->request($MerchantID, $Amount, $Description, $Email, $Mobile, $CallbackURL, $SandBox, $ZarinGate);
+if (isset($result["Status"]) && $result["Status"] == 100) {
+	// Success and redirect to pay
+	$zp->redirect($result["StartPay"]);
+} else {
+	// error
+	echo "خطا در ایجاد تراکنش";
+	echo "<br />کد خطا : " . $result["Status"];
+	echo "<br />تفسیر و علت خطا : " . $result["Message"];
+}
---
+++ Verify.php
@@ -0,0 +1,20 @@
+<?php
+require_once("ZarinpalFunction.php");
+$MerchantID 		= "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+$Amount 			= 100;
+$ZarinGate 		= false;
+$SandBox 			= false;
+$zp 	= new zarinpal();
+$result = $zp->verify($MerchantID, $Amount, $SandBox, $ZarinGate);
+if (isset($result["Status"]) && $result["Status"] == 100) {
+	// Success
+	echo "تراکنش با موفقیت انجام شد";
+	echo "<br />مبلغ : " . $result["Amount"];
+	echo "<br />کد پیگیری : " . $result["RefID"];
+	echo "<br />Authority : " . $result["Authority"];
+} else {
+	// error
+	echo "پرداخت ناموفق";
+	echo "<br />کد خطا : " . $result["Status"];
+	echo "<br />تفسیر و علت خطا : " . $result["Message"];
+}
